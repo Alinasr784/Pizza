09d095908c9281ab7a2b245f5aec653fea4baabf
Ali Nasreldin Ali Mohammed <alinasreldin784@gmail.com>
2024-10-28 02:45:59 +0000

    handle landing page in mobile phone screen

--- project/app/src/js/landing.js
+++ project/app/src/js/landing.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import '../css/landing.css'
+if (window.innerWidth > 599) {
+  import('../css/landing-tablet.css')
+} else {
+  import('../css/landing-phone.css')
+}
 function Text() {
   return (
     <div className="text">
-      <div className="title">Let's Pizza Time</div>
+      <div className="title">Let<div>'s</div>Pizza Time</div>
       <div className="des">Welcome to Let's Pizza Time, your ultimate destination for pizza lovers! We offer a diverse range of the most delicious pizzas, made from fresh, high-quality ingredients.</div>
     </div>
   )
@@ -31,14 +35,34 @@ function Abs() {
     </div>
   )
 }
+
 function Landing() {
+  const ui = () => {
+    if (window.innerWidth > 599) {
+      return (
+        <div className="landing">
+          <div className="overlay">
+            <Text />
+            <Btns />
+            <Abs />
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="landing">
+          <div className="overlay">
+            <Text />
+            <Btns />
+            <Abs />
+          </div>
+        </div>
+      )
+    }
+  }
   return (
-    <div className="landing">
-      <div className="overlay">
-        <Text />
-        <Btns />
-        <Abs />
-      </div>
+    <div>
+      {ui()}
     </div>
   )
 }

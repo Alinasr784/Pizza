f91a9d00a6eea71af0878221bc49f214482fea55
Ali Nasreldin Ali <157469044+Alinasr784@users.noreply.github.com>
2024-10-29 04:45:48 +0000

    the third screen has done, card screen has done in mobile phone and tablet screens

--- project/app/src/index.js
+++ project/app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import Header from './js/header'
 import Landing from './js/landing'
 import Apps from './js/apps'
+import CardPage from './js/cards'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <Landing />
     <Apps />
+    <CardPage />
   </React.StrictMode>
 );
--- project/app/src/js/cards.js
+++ project/app/src/js/cards.js
@@ -1,5 +1,9 @@
 import React from 'react'
-
+if (window.innerWidth > 599) {
+  import('../css/cards-tablet.css')
+} else {
+  import('../css/cards-phone.css')
+}
 function Title() {
   return (
     <div>
@@ -16,31 +20,41 @@ function Des() {
 }
 function Card(props) {
   return (
-    <div>
-      <div className="card">
+      <div className="card" id={props.id}>
         <div className="icon">
-          <img src={props.icon} />
+          <img src={props.icon} alt="icon"/>
         </div>
         <div className="card-title">{props.cardTitle}</div>
         <div className="card-des">{props.cardDes}</div>
         <div className="card-btn">
-          <div className="card-btn-text">{props.cardBtnText}</div>
+          <div className="card-btn-text">Explore Menu</div>
           <div className="card-btn-icon">
-            <img src={props.cardBtnIcon} />
+            <img src='/arrow.svg' />
           </div>
         </div>
       </div>
+  )
+}
+function Btns() {
+  return (
+    <div className="btnsCards">
+      <div className="order">Order Online</div>
+      <div className="book">Book a Table</div>
     </div>
   )
 }
 function CardsPage(){
   return(
-    <div>
+    <div className="cardPage">
       <Title />
       <Des />
-      <div>
-        <Card icon={"#"} cardTitle={"#"} cardDes={"#"} cardBtnText={"#"} cardBtnIcon={"#"}/>
+      <div className="cardSection">
+        <Card icon={'/icon1.svg'} id={"card1"} cardTitle={'Breakfast'} cardDes={'Breakfast is the most impor tant meal of the day, Breakfast is the most impor tant meal '}/>
+        <Card icon={'/icon2.svg'} id={"card2"} cardTitle={'Main Dishes'} cardDes={'Breakfast is the most impor tant meal of the day, Breakfast is the most impor tant meal '}/>
+        <Card icon={'/icon3.svg'} id={"card3"} cardTitle={'Drinks'} cardDes={'Breakfast is the most impor tant meal of the day, Breakfast is the most impor tant meal '}/>
+        <Card icon={'/icon4.svg'} id={"card4"} cardTitle={'Desserts'} cardDes={'Breakfast is the most impor tant meal of the day, Breakfast is the most impor tant meal '}/>
       </div>
+      <Btns />
     </div>
   )
 }
